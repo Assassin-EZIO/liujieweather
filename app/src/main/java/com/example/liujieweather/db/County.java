d89343d59926549eab1a92347a280d7508b5c6df
LiuJie <1141988108@qq.com>
2018-03-25 20:38:33 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/liujieweather/db/County.java
@@ -0,0 +1,50 @@
+package com.example.liujieweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 11419 on 2018/3/25.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+
+    private String countyName;
+
+    private String weatherId;
+
+    private int countyCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(int countyCode) {
+        this.countyCode = countyCode;
+    }
+}
