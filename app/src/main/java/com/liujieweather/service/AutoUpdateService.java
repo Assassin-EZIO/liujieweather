8c0307ffe8a2db6dca9f79f94017b72110b91395
LiuJie <1141988108@qq.com>
2018-03-26 20:24:12 +0800

    修改程序图标和名称

--- app/src/main/java/com/liujieweather/service/AutoUpdateService.java
+++ app/src/main/java/com/liujieweather/service/AutoUpdateService.java
@@ -32,11 +32,11 @@ public class AutoUpdateService extends Service {
         updateBingPic();
         AlarmManager manager=(AlarmManager)getSystemService(ALARM_SERVICE);
         int anHour=8*60*60*1000;
-        long triggerTime= SystemClock.elapsedRealtime()+anHour;
+        long triggerAtTime= SystemClock.elapsedRealtime()+anHour;
         Intent i=new Intent(this,AutoUpdateService.class);
         PendingIntent pi= PendingIntent.getService(this,0,i,0);
         manager.cancel(pi);
-        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,triggerTime,pi);
+        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,triggerAtTime,pi);
         return super.onStartCommand(intent,flags,startId);
     }
 
